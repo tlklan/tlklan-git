1f583e076c93a53838939b9619f3c87b128470cb
Sam <Sam@Sam-PC.negge.fi>
2012-09-29 13:40:32 +0300

    general cleanup and reformat

--- protected/models/Registration.php
+++ protected/models/Registration.php
@@ -22,11 +22,12 @@
  */
 class Registration extends CActiveRecord
 {
+
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @return Registration the static model class
 	 */
-	public static function model($className=__CLASS__)
+	public static function model($className = __CLASS__)
 	{
 		return parent::model($className);
 	}
@@ -47,14 +48,17 @@ class Registration extends CActiveRecord
 		return array(
 			array('lan_id, name, email, nick, device, date', 'required'),
 			array('lan_id', 'numerical', 'integerOnly'=>true),
-			//array('id, lan_id, name, email, nick, device, date, confirmed, deleted', 'safe', 'on'=>'search'),
 		);
 	}
 
-	protected function afterConstruct() {
+	/**
+	 * Sets some default values for new objects
+	 */
+	protected function afterConstruct()
+	{
 		$this->confirmed = false;
 		$this->deleted = false;
-		
+
 		parent::afterConstruct();
 	}
 
@@ -63,18 +67,20 @@ class Registration extends CActiveRecord
 	 * 
 	 * @return array the default scope 
 	 */
-	public function defaultScope() {
+	public function defaultScope()
+	{
 		return array(
 			// Order newest first
 			'order'=>'id DESC',
 		);
 	}
-	
+
 	/**
 	 * Returns the scopes for this model.
 	 * @return array the scope definitions
 	 */
-	public function scopes() {
+	public function scopes()
+	{
 		return array(
 			'currentLan'=>array(
 				'condition'=>'lan_id = '.Lan::model()->getCurrent()->id,
@@ -88,10 +94,10 @@ class Registration extends CActiveRecord
 	public function relations()
 	{
 		return array(
-			'competitions' => array(self::HAS_MANY, 'Competitor', 'registration_id'),
-			'lan' => array(self::BELONGS_TO, 'Lans', 'lan_id'),
-			'results' => array(self::HAS_MANY, 'Results', 'reg_id'), // TODO: What the hell is the results table?
-			'votes' => array(self::HAS_MANY, 'Vote', 'voter_id'),
+			'competitions'=>array(self::HAS_MANY, 'Competitor', 'registration_id'),
+			'lan'=>array(self::BELONGS_TO, 'Lans', 'lan_id'),
+			'results'=>array(self::HAS_MANY, 'Results', 'reg_id'),
+			'votes'=>array(self::HAS_MANY, 'Vote', 'voter_id'),
 		);
 	}
 
@@ -101,67 +107,45 @@ class Registration extends CActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'id' => 'ID',
-			'lan_id' => 'Lan',
-			'name' => 'Name',
-			'email' => 'Email',
-			'nick' => 'Nick',
-			'device' => 'Device',
-			'date' => 'Date',
-			'confirmed' => 'Confirmed',
-			'deleted' => 'Deleted',
+			'id'=>'ID',
+			'lan_id'=>'Lan',
+			'name'=>'Name',
+			'email'=>'Email',
+			'nick'=>'Nick',
+			'device'=>'Device',
+			'date'=>'Date',
+			'confirmed'=>'Confirmed',
+			'deleted'=>'Deleted',
 		);
 	}
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('id',$this->id);
-		$criteria->compare('lan_id',$this->lan_id);
-		$criteria->compare('name',$this->name,true);
-		$criteria->compare('email',$this->email,true);
-		$criteria->compare('nick',$this->nick,true);
-		$criteria->compare('device',$this->device,true);
-		$criteria->compare('date',$this->date,true);
-		$criteria->compare('confirmed',$this->confirmed);
-		$criteria->compare('deleted',$this->deleted);
-
-		return new CActiveDataProvider(get_class($this), array(
-			'criteria'=>$criteria,
-		));
-	}
-	
 	/**
 	 * Returns a list of all registered people for the specified LAN
 	 * 
 	 * @param int $lanId the LAN
 	 * @return array the registered people 
 	 */
-	public function findByLAN($lanId) {
+	public function findByLAN($lanId)
+	{
 		return $this->findAll('lan_id = :lan_id', array('lan_id'=>$lanId));
 	}
-	
+
 	/**
 	 * Returns true if the user is using a laptop
 	 * @return boolean 
 	 */
-	public function hasLaptop() {
+	public function hasLaptop()
+	{
 		return $this->device == 'laptop';
 	}
-	
+
 	/**
 	 * Returns true if the user is using a desktop
 	 * @return boolean 
 	 */
-	public function hasDesktop() {
+	public function hasDesktop()
+	{
 		return $this->device == 'desktop';
 	}
+
 }
--- protected/widgets/registration/RegistrationListWidget.php
+++ protected/widgets/registration/RegistrationListWidget.php
@@ -5,34 +5,42 @@
  *
  * @author Sam
  */
-class RegistrationListWidget extends CWidget {
-	public $currentLan;
+class RegistrationListWidget extends CWidget 
+{
 	
 	/**
-	 * All registrations for this LAN
-	 * @var array
+	 * @var Lan the current LAN
+	 */
+	public $currentLan;
+
+	/**
+	 * @var array All registrations for this LAN
 	 */
 	public $registrations;
-	
+
 	/**
-	 * All competitions for this LAN
-	 * @var array 
+	 * @var array All competitions for this LAN
 	 */
 	public $competitions;
-	
+
+	/**
+	 * @var int the number of registrations for this LAN
+	 */
 	private $_registrationCount;
-	
+
 	/**
 	 * Initializes the widget
 	 */
-	public function init() {
+	public function init()
+	{
 		$this->_registrationCount = count($this->registrations);
 	}
 
 	/**
 	 * Runs the widget. The rendering is done here.
 	 */
-	public function run() {
+	public function run() 
+	{
 		ob_start();
 		
 		?>
@@ -40,9 +48,8 @@ class RegistrationListWidget extends CWidget {
 			<?php
 			
 			$this->renderRegistrationCount();
-			if($this->_registrationCount > 0) {
+			if ($this->_registrationCount > 0)
 				$this->renderList();
-			}
 			
 			?>
 		</div>
@@ -54,7 +61,8 @@ class RegistrationListWidget extends CWidget {
 	/**
 	 * Renders the actual list
 	 */
-	private function renderList() {
+	private function renderList() 
+	{
 		ob_start();
 		
 		// Get the user object
@@ -83,7 +91,8 @@ class RegistrationListWidget extends CWidget {
 				<?php
 
 				// Add one column for each competition
-				foreach($this->competitions as $competition) {
+				foreach($this->competitions as $competition) 
+				{
 					?><th class="compo_header"><?php echo $competition->short_name; ?>:</th><?php
 				}
 
@@ -93,16 +102,16 @@ class RegistrationListWidget extends CWidget {
 			<?php
 
 			// Print each row
-			foreach($this->registrations as $registration) {
+			foreach($this->registrations as $registration) 
+			{
 				$name = CHtml::encode($registration->name);
 				$nick = CHtml::encode($registration->nick);
 				$registeredCompetitions = array();
 				
 				// Get a list of the IDs of the competitions the current user
 				// is registered to
-				foreach($registration->competitions as $competition) {
+				foreach($registration->competitions as $competition)
 					$registeredCompetitions[] = $competition->competition_id;
-				}
 				
 				?>
 				<tr>
@@ -116,7 +125,8 @@ class RegistrationListWidget extends CWidget {
 						// for administrators
 						$isOwner = strtolower($registration->nick) === strtolower($user->nick);
 						
-						if($isOwner || $user->isAdmin()) {
+						if($isOwner || $user->isAdmin()) 
+						{
 							echo CHtml::link(
 								CHtml::image(
 									Yii::app()->baseUrl.'/files/images/icons/edit_button.png'
@@ -127,7 +137,8 @@ class RegistrationListWidget extends CWidget {
 						}
 						
 						// Show delete link for administrators
-						if($user->isAdmin()) {
+						if($user->isAdmin()) 
+						{
 							echo CHtml::link(
 								CHtml::image(Yii::app()->baseUrl.'/files/images/icons/delete_button.png'), 
 								Yii::app()->controller->createUrl('registration/delete', array('id'=>$registration->id)),
@@ -157,7 +168,8 @@ class RegistrationListWidget extends CWidget {
 					</td>
 					<?php
 
-					foreach($this->competitions as $competition) {
+					foreach($this->competitions as $competition) 
+					{
 						?>
 						<td class="center-align">
 							<?php echo (in_array($competition->id, $registeredCompetitions)) ? 'x' : ''; ?>
@@ -183,7 +195,8 @@ class RegistrationListWidget extends CWidget {
 	/**
 	 * Prints the amount of registered people
 	 */
-	private function renderRegistrationCount() {
+	private function renderRegistrationCount() 
+	{
 		ob_start();
 		
 		?>
