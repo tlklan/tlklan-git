16fe23dc2e3b08f93a5e587c98647efb60c242dc
Sam <Sam@Sam-PC.negge.fi>
2012-10-03 01:57:03 +0300

    created a controller action for voting

---
+++ protected/controllers/VoteController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Description of VoteController
+ *
+ * @author Sam Stenvall <sam@supportersplace.com>
+ */
+class VoteController extends Controller
+{
+
+	public function actionCreate()
+	{
+		$this->render('create');
+	}
+
+}
---
+++ protected/views/vote/create.php
@@ -0,0 +1,11 @@
+<?php
+
+$this->pageTitle = 'Rösta';
+$this->breadcrumbs=array(
+	'Rösta',
+);
+
+?>
+<h1>Rösta</h1>
+
+<?php $this->widget('cms.widgets.CmsBlock',array('name'=>'vote-info')); ?>
