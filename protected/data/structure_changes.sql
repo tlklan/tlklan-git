e83397b4ad5348b4783badb88ad00eb48c494a10
Sam <Sam@Jeltsin.negge.fi>
2012-11-06 18:22:14 +0200

    - added users table and corresponding model - added user_id column and foreign key constraint to tlk_registrations

--- protected/data/structure_changes.sql
+++ protected/data/structure_changes.sql
@@ -47,3 +47,40 @@ ENGINE=InnoDB;
 # for the competition
 ALTER TABLE `tlk_competitions`
 	ADD COLUMN `signupable` TINYINT(1) NOT NULL DEFAULT '0' AFTER `votable`;
+
+#
+# 2012-11-06
+#
+# Added users table
+CREATE TABLE `tlk_users` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(75) NOT NULL,
+	`email` VARCHAR(50) NOT NULL,
+	`username` VARCHAR(25) NOT NULL,
+	`password` CHAR(32) NULL DEFAULT NULL,
+	`has_werket_login` TINYINT(1) NOT NULL DEFAULT '0',
+	`date_added` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	INDEX `username` (`username`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=512;
+
+# Change collation on tlk_registrations and its columns
+ALTER TABLE `tlk_registrations`
+	COLLATE='utf8_general_ci';
+ALTER TABLE `tlk_registrations`
+	ALTER `name` DROP DEFAULT,
+	ALTER `email` DROP DEFAULT,
+	ALTER `nick` DROP DEFAULT,
+	ALTER `device` DROP DEFAULT;
+ALTER TABLE `tlk_registrations`
+	CHANGE COLUMN `name` `name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci' AFTER `user_id`,
+	CHANGE COLUMN `email` `email` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci' AFTER `name`,
+	CHANGE COLUMN `nick` `nick` VARCHAR(30) NOT NULL COLLATE 'utf8_general_ci' AFTER `email`,
+	CHANGE COLUMN `device` `device` ENUM('desktop','laptop') NOT NULL COLLATE 'utf8_general_ci' AFTER `nick`;
+
+# Add foreign key constraints between tlk_users and tlk_registration
+ALTER TABLE `tlk_registrations`
+	ADD CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `tlk_users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
---
+++ protected/models/User.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This is the model class for table "tlk_users".
+ *
+ * The followings are the available columns in table 'tlk_users':
+ * @property integer $id
+ * @property string $name
+ * @property string $username
+ * @property string $password
+ * @property integer $has_werket_login
+ * @property string $date_added
+ */
+class User extends CActiveRecord
+{
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return User the static model class
+	 */
+	public static function model($className = __CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'tlk_users';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return array(
+			array('name, username, password, date_added', 'required'),
+			array('has_werket_login', 'numerical', 'integerOnly'=>true),
+			array('name', 'length', 'max'=>75),
+			array('username', 'length', 'max'=>25),
+			array('id, name, username, password, has_werket_login, date_added', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		return array(
+		
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id'=>'ID',
+			'name'=>'Namn',
+			'username'=>'Användarnamn',
+			'has_werket_login'=>'Har Werket-konto',
+			'date_added'=>'Datum skapad',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models 
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		$criteria = new CDbCriteria;
+		$criteria->compare('id', $this->id);
+		$criteria->compare('name', $this->name, true);
+		$criteria->compare('username', $this->username, true);
+		$criteria->compare('password', $this->password, true);
+		$criteria->compare('has_werket_login', $this->has_werket_login);
+		$criteria->compare('date_added', $this->date_added, true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+}
