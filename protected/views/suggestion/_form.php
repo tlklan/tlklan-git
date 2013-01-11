a8b168ab8765ff029a3a8559ac50cb6e0b416c1e
Sam <Sam@Jeltsin.negge.fi>
2013-01-11 20:33:42 +0200

    added Cancel button and better description for the description field

--- protected/views/suggestion/_form.php
+++ protected/views/suggestion/_form.php
@@ -12,7 +12,7 @@ echo $form->textFieldRow($model, 'name', array(
 echo $form->textAreaRow($model, 'description', array(
 	'class'=>'span4', 
 	'style'=>'min-height: 150px;',
-	'hint'=>'<i>Förklara kort vad tävlingen skulle gå ut på</i>'));
+	'hint'=>'<i>Förklara kort vad tävlingen skulle gå ut på. Om du vill utveckla ditt svar vid ett senare tillfälle kan du ändra beskrivningen genom att trycka på penikonen i listan nedan.</i>'));
 
 ?>
 <div class="form-actions">
@@ -21,5 +21,11 @@ echo $form->textAreaRow($model, 'description', array(
 		'type'=>'primary',
 		'icon'=>'ok white',
 		'label'=>$model->isNewRecord ? 'Lämna in förslaget' : 'Uppdatera',
+	)); ?>&nbsp;&nbsp;
+	<?php $this->widget('bootstrap.widgets.TbButton', array(
+		'buttonType'=>'link',
+		'icon'=>'remove',
+		'label'=>'Avbryt',
+		'url'=>$this->createUrl('/suggestion/create'),
 	)); ?>
 </div>
