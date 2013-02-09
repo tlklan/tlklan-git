7e778580e65531a8ec3a81f8bdb0d9ed3b58810c
Sam <Sam@Jeltsin.negge.fi>
2013-02-09 17:36:37 +0200

    render the list of devices manually. This way we can finally disable the iPad option (the way it was before the Yii rewrite)

---
+++ protected/views/registration/_deviceList.php
@@ -0,0 +1,48 @@
+<?php
+
+/* @var $form TbActiveForm */
+/* @var $model RegistrationForm */
+
+?>
+<div class="control-group">
+	<?php echo $form->labelEx($model, 'device', array('class'=>'control-label')); ?>
+	
+	<div class="controls">
+		<?php
+		
+		$attribute = 'device';
+		$i = 0;
+		
+		foreach(Registration::$validDevices as $value => $name) 
+		{
+			// Determine initial htmlOptions for the label and radiobutton
+			$htmlOptions = array();
+			CHtml::resolveNameID($model, $attribute, $htmlOptions);
+
+			// Make a different set of options for the radio button
+			$htmlOptions['id'] .= '_'.$i;
+			$radioHtmlOptions = $htmlOptions;
+			$radioHtmlOptions['value'] = $value;
+			$radioHtmlOptions['uncheckValue'] = null;
+
+			// Check the used device
+			if ($model->device)
+				$radioHtmlOptions['checked'] = $value == $model->device;
+
+			// Disable the iPad option
+			if ($value == 'ipad')
+				$radioHtmlOptions['disabled'] = true;
+			
+			?>
+			<label class="radio">
+				<?php echo $form->radioButton($model, 'device', $radioHtmlOptions); ?>
+				<?php echo CHtml::label($name, $htmlOptions['id'], $htmlOptions); ?>
+			</label>
+			<?php
+			
+			$i++;
+		}
+		
+		?>
+	</div>
+</div>
