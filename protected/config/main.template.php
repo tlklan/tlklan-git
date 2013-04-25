1864e3c929e683981563f37a6e4d7520582110e9
Sam Stenvall <devnull@localhost>
2013-04-25 23:37:51 +0300

    added separate sub-folder for badge models

--- protected/config/main.template.php
+++ protected/config/main.template.php
@@ -19,6 +19,7 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.models.forms.*',
+		'application.models.badges.*',
 		'application.components.*',
 		'application.components.behaviors.*',
 		'application.widgets.*',
