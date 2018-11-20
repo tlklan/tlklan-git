3e4d071bffb479eba7c6ec1e78b98a3030ad12e4
Sam Stenvall <sam.stenvall@nordsoftware.com>
2018-11-20 21:32:51 +0200

    Update some apt package names

--- provisioning/01-install-system-packages.sh
+++ provisioning/01-install-system-packages.sh
@@ -9,7 +9,7 @@ set -e
 	debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 	debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
-	apt-get -y install php5-cli mysql-server php5-mysql apache2 libapache2-mod-php5 unzip
+	apt-get -y install php7.2-cli mysql-server php7.2-mysql apache2 libapache2-mod-php7.2 unzip
 	
 	wget https://getcomposer.org/download/1.7.3/composer.phar -O /usr/local/bin/composer
 	chmod +x /usr/local/bin/composer
