1774a616e7d9da359871a887815a1c1019a9b042
Sam Stenvall <sam.stenvall@nordsoftware.com>
2018-11-20 20:29:41 +0200

    Install Composer in the Vagrant box

--- provisioning/01-install-system-packages.sh
+++ provisioning/01-install-system-packages.sh
@@ -8,6 +8,9 @@
 	debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
 	apt-get -y install php5-cli mysql-server php5-mysql apache2 libapache2-mod-php5 unzip
+	
+	wget https://getcomposer.org/download/1.7.3/composer.phar -O /usr/local/bin/composer
+	chmod +x /usr/local/bin/composer
 }
 
 # > /dev/null 2>&1
