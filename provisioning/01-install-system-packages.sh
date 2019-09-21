273a41a05bf0363aa2054da0f1474936eb867c45
Sam Stenvall <sam.stenvall@digia.com>
2019-09-21 10:24:18 +0300

    Install GD in Vagrant so image resizing works

--- provisioning/01-install-system-packages.sh
+++ provisioning/01-install-system-packages.sh
@@ -9,7 +9,7 @@ set -e
 	debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 	debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
-	apt-get -y install php7.2-cli mysql-server php7.2-mysql apache2 libapache2-mod-php7.2 unzip
+	apt-get -y install php7.2-cli mysql-server php7.2-mysql apache2 libapache2-mod-php7.2 unzip php7.2-gd
 	
 	wget https://getcomposer.org/download/1.7.3/composer.phar -O /usr/local/bin/composer
 	chmod +x /usr/local/bin/composer
