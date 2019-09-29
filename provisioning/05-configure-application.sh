714f1060eb3c5070894a89ad7f7a70f1029273fa
Sam Stenvall <sam.stenvall@digia.com>
2019-09-29 18:19:37 +0300

    Remove obsolete main.php installation from the provisioning scripts

--- provisioning/05-configure-application.sh
+++ provisioning/05-configure-application.sh
@@ -3,11 +3,8 @@
 set -e
 
 {
-	# create the main configuration file
-	cp -f /vagrant/provisioning/config/main.php /vagrant/protected/config
-
-    # change ownership of /media
-    chown vagrant:vagrant /media
+	# change ownership of /media
+	chown vagrant:vagrant /media
 
 	# create a directory and a symlink for submissions (path is hardcoded in the database)
 	if [ ! -d /media/Storage ]
