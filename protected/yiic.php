ba31372ce59c002bfde1225c48b2dfb8fccf102e
Sam Stenvall <sam.stenvall@digia.com>
2019-10-04 15:07:14 +0300

    Add missing .env file loading to yiic.php

--- protected/yiic.php
+++ protected/yiic.php
@@ -2,6 +2,13 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+// Load environment variables from .env if present
+if (is_readable(__DIR__ . '/../.env')) {
+	Dotenv::create(__DIR__.'/../')->load();
+}
+
 $yiic= __DIR__ .'/../vendor/yiisoft/yii/framework/yiic.php';
 
 $config = require __DIR__.'/config/console.php';
